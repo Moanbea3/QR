dcba83878e729a12ea518aacd306df6c5b7b613a
Edgar M. Rios <eds.mrios@gmail.com>
2023-08-20 16:09:13 -0400

    refactor: type en ResultadoPago siempre es 'success'

--- src/components/ResultadoPago.jsx
+++ src/components/ResultadoPago.jsx
@@ -2,11 +2,11 @@ import { Link, useLocation } from 'react-router-dom'
 
 export const ResultadoPago = () => {
   const location = useLocation()
-  const { message, type = 'success' } = location.state
+  const { message } = location.state
 
   return (
     <>
-      <h2 className={type}>{message}</h2>
+      <h2 className="success">{message}</h2>
       <Link to="/">
         <button className="volver">Volver a inicio</button>
       </Link>
